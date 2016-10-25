24c1854cd701843c83fc9e7712f5787a8fdfaa48
PolexRufous <polex.rufous@gmail.com>
2016-10-25 21:31:10 +0300

    Correct Vehicle and VehicleType entities

--- src/main/java/com/flashlogist/database/entities/vehicle/Vehicle.java
+++ src/main/java/com/flashlogist/database/entities/vehicle/Vehicle.java
@@ -1,13 +1,52 @@
 package com.flashlogist.database.entities.vehicle;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 
-import java.time.LocalDate;
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.sql.Date;
 
-public @Data
-class Vehicle {
+
+@Data
+@Entity
+@Table(name = "GLOBAL_USER", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "id"),
+        @UniqueConstraint(columnNames = "name"),
+        @UniqueConstraint(columnNames = "regnumber")
+})
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @NotEmpty(message="Name cannot be empty")
+    @NotNull(message="Name cannot be null")
+    @Pattern(regexp="[A-Za-z0-9_]+", message="Use only ASCII letters, digits and _ simbol")
+    @Size(min = 6, max = 24, message="Name cannot be less than {min} letters and longer than {max} symbols")
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
+
+    @NotEmpty(message="Registration number cannot be empty")
+    @NotNull(message="Registration number cannot be null")
+    @Size(min = 4, max = 24, message="Registration number cannot be less than {min} letters and longer than {max} symbols")
+    @Column(name = "regnumber", unique = true, nullable = false)
+    private String regnumber;
+
+    @ManyToOne
+    @Valid
+    @JoinColumn(name="type")
     private VehicleType type;
-    private LocalDate manufacturingDate;
+
+    @Past(message="Manufactoring date cannot be future")
+    @Column(name = "manufacturingdate")
+    private Date manufacturingDate;
+
+
+    @Min(value = 0, message = "Max cargo weight must be positive")
+    @Column(name = "maxcargoweight")
+    private int maxcargoweight;
 }
--- src/main/java/com/flashlogist/database/entities/vehicle/VehicleType.java
+++ src/main/java/com/flashlogist/database/entities/vehicle/VehicleType.java
@@ -1,5 +1,34 @@
 package com.flashlogist.database.entities.vehicle;
 
-public enum VehicleType {
-    TRUCK, CAR, SHIP
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Table(name = "GLOBAL_VEHICLE_TYPE", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "id"),
+        @UniqueConstraint(columnNames = "name"),
+})
+public class VehicleType {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name cannot be empty")
+    @NotNull(message = "Name cannot be null")
+    @Pattern(regexp = "[A-Za-z]+", message = "Use only ASCII letters")
+    @Size(min = 1, max = 24, message = "Name cannot be less than {min} letter and longer than {max} lettets")
+    private String name;
+
+    @Column(name = "description")
+    @Size(max = 24, message = "Description cannot be longer than {max} lettets")
+    private String description;
 }
