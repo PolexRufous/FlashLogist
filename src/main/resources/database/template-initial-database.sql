b464c484c04e5d7cf6fa4e0ca3873432bc4cee96
PolexRufous <polex.rufous@gmail.com>
2016-10-30 18:33:07 +0200

    Delete trash classes and files. Change init database template

--- src/main/resources/database/template-initial-database.sql
+++ src/main/resources/database/template-initial-database.sql
@@ -1,5 +1,2 @@
-INSERT INTO GLOBAL_USER_ROLE (id, name) VALUES (1, 'USER');
-INSERT INTO GLOBAL_USER_ROLE (id, name) VALUES (2, 'ADMIN');
-INSERT INTO GLOBAL_USER (id, login, password, available, firstname, lastname, email, role) VALUES (1, 'user', 'user', true, 'User', 'Userovich', 'user@email.com', 1);
-INSERT INTO GLOBAL_USER (id, login, password, available, firstname, lastname, email, role) VALUES (2, 'admin', 'admin', true, 'Admin', 'Adminin', 'admin@email.com', 2);
-INSERT INTO TEST_SOME_CLASS (id, one, two) VALUES (1, 'one', 'two');
+INSERT INTO GLOBAL_USER (id, login, password, available, firstname, lastname, email, role) VALUES (1, ''user'', ''user'', true, ''User'', ''Userovich'', ''user@email.com'', ''USER'');
+INSERT INTO GLOBAL_USER (id, login, password, available, firstname, lastname, email, role) VALUES (2, ''admin'', ''admin'', true, ''Admin'', ''Adminin'', ''admin@email.com'', ''ADMIN'');
