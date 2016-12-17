88201ff81b902d99d977b3ba8c9a490ec26ab22c
Aleksandr Kuschev <polex.rufous@gmail.com>
2016-12-18 00:41:05 +0200

    Styling

--- src/main/webapp/resources/global/js/header.base.js
+++ src/main/webapp/resources/global/js/header.base.js
@@ -2,21 +2,12 @@ window.onload = function () {
     $('.menu-container').find(' .parent').on('click', function (event) {
         var element = event.currentTarget;
         element = $(element).find('.container');
-        if (element.is(':visible')) {
-            //element.hide();
-        } else {
-            element.show();
-        }
+        element.addClass("current");
     });
 
-   /* $('body').on('click', function (event) {
-        var element = $(event.currentTarget);
-        $('.menu-container').find('.parent').find('.container').each(function () {
-            if (element != this) {
-                if ($(this).is(':visible')) {
-                    $(this).hide();
-                }
-            }
-        });
-    });*/
+    $('body').on('click', function (event) {
+        if (!$(event.target).hasClass('menu-span')) {
+            $('.container.current').removeClass('current');
+        }
+    });
 };
