3796a2bf31fcbf187f16393f094897825385d2d1
Aleksandr Kuschev <polex.rufous@gmail.com>
2016-12-21 00:44:38 +0200

    Stile pupup menu

--- src/main/webapp/resources/global/js/header.base.js
+++ src/main/webapp/resources/global/js/header.base.js
@@ -1,13 +1,32 @@
 window.onload = function () {
-    $('.menu-container').find(' .parent').on('click', function (event) {
-        var element = event.currentTarget;
-        element = $(element).find('.container');
-        element.addClass("current");
+    $('.menu-container').find('.parent-span').on('click', function (event) {
+        var element = $(event.currentTarget);
+        var showingMenu = $(element.siblings('.container')[0]);
+        if (showingMenu.hasClass('current')) {
+            showingMenu.removeClass('current');
+        } else {
+            showingMenu.addClass("current");
+            slowAppear(showingMenu);
+        }
     });
 
     $('body').on('click', function (event) {
-        if (!$(event.target).hasClass('menu-span')) {
+        var element = $(event.target);
+        if (element.parents('.parent').length == 0) {
             $('.container.current').removeClass('current');
         }
     });
+
+    function slowAppear(jQElement) {
+        jQElement.css('opacity', 0.0);
+        var intervalId = setInterval(function () {
+            var currentOpacity = jQElement.css('opacity');
+            currentOpacity = parseFloat(currentOpacity);
+            if (currentOpacity < 1.0) {
+                jQElement.css('opacity', currentOpacity + 0.1);
+            } else {
+                clearInterval(intervalId);
+            }
+        }, 100);
+    }
 };
